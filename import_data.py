0f4e0332630737e14f0d5d2bdabe4a00679f2862
loanerabillard <loane.rabillard@efrei.net>
2023-10-30 09:08:58 +0100

    final

--- import_data.py
+++ import_data.py
@@ -3,13 +3,12 @@ import pandas as pd
 import geopandas as gpd
 
 
-@st.cache
 def load_data():
     df = pd.read_csv('donnee-dep-data.gouv-2022-geographie2023-produit-le2023-07-17.csv', delimiter=';')
     #df['annee'] = df['annee'].astype(str)
     return df
 
-@st.cache
+
 def géo_load():
     departements = gpd.read_file("departements.geojson")
     return departements
